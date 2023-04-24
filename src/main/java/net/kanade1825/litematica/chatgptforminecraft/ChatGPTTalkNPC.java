d5747011b4212d3b077e0a44ebb26721792a28a2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-25 01:39:08 +0900

    wef

--- src/main/java/net/kanade1825/litematica/chatgptforminecraft/ChatGPTTalkNPC.java
+++ src/main/java/net/kanade1825/litematica/chatgptforminecraft/ChatGPTTalkNPC.java
@@ -12,8 +12,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,35 +34,55 @@ public class ChatGPTTalkNPC implements Listener {
     private void loadNpcData() {
         List<String> npcNames = List.of("Wagner", "Mirai", "Ellis", "Rina", "Rasvaan", "Raisers", "Marshier");
 
+        System.out.println(1);
+
         for (String npcName : npcNames) {
-            try (FileReader reader = new FileReader(npcName + ".json")) {
+
+            try (InputStreamReader reader = new InputStreamReader(new FileInputStream(npcName + ".json"), StandardCharsets.UTF_8)) {
+
                 JSONObject jsonObject = (JSONObject) new JSONParser().parse(reader);
+
                 String data = (String) jsonObject.get(npcName);
+
                 npcData.put(npcName, data);
+
             } catch (IOException | ParseException e) {
+
                 e.printStackTrace();
+
             }
         }
     }
 
     @EventHandler
     public void onNPCRightClick(NPCRightClickEvent event) {
+
         Player player = event.getClicker();
+
         NPC npc = event.getNPC();
+
         String npcName = npc.getName();
 
+
         if (npcData.containsKey(npcName)) {
+
             Bukkit.getScheduler().runTaskAsynchronously(chatGptForMinecraft, () -> {
+
                 List<ChatMessage> chatMessages = new LinkedList<>();
+
                 chatMessages.add(new ChatMessage("user", npcData.get(npcName)));
+
                 chatMessages.add(new ChatMessage("user", "こんにちは！"));
+
 
                 final var completionRequest = ChatCompletionRequest.builder()
                         .model("gpt-3.5-turbo")
                         .messages(chatMessages)
                         .build();
 
+
                 String answer = String.valueOf(chatGptForMinecraft.getService().createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent());
+
                 player.sendMessage(answer);
             });
         }
